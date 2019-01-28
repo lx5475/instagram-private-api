15f021547a63605946f7541087c6f3c18e012a03
lx5475 <lx5475@gmail.com>
2019-01-28 18:31:28 +0900

    다른 모듈도 업데이트

--- client/v1/constants.js
+++ client/v1/constants.js
@@ -14,6 +14,7 @@ const ROUTES = {
     comment: 'media/<%= id %>/comment/',
     commentDelete: 'media/<%= id %>/comment/<%= commentId %>/delete/',
     commentBulkDelete: 'media/<%= id %>/comment/bulk_delete/',
+    commentLike: 'media/<%= id %>/comment_like/',
     like: 'media/<%= id %>/like/',
     unlike: 'media/<%= id %>/unlike/',
     registrationCreate: 'accounts/create/',
@@ -29,11 +30,13 @@ const ROUTES = {
     friendshipShowMany: 'friendships/show_many/',
     friendshipPending: 'friendships/pending/',
     friendshipPendingApprove: 'friendships/approve/<%= id %>/',
+    friendshipRemoveFollower: 'friendships/remove_follower/<%= id %>/',
     userInfo: 'users/<%= id %>/info/',
     userFeed: 'feed/user/<%= id %>/?<%= maxId ? ("max_id=" + maxId + "&") : "" %>rank_token=<%= rankToken %>',
-    timelineFeed: 'feed/timeline/?<%= maxId ? ("max_id=" + maxId + "&") : "" %>rank_token=<%= rankToken %>&ranked_content=true',
+    timelineFeed: 'feed/timeline/<%= maxId ? ("?max_id=" + maxId) : "" %>',
     tagFeed: 'feed/tag/<%= encodeURI(tag) %>/?<%= maxId ? ("max_id=" + maxId + "&") : "" %>rank_token=<%= rankToken %>',
     selfLikedFeed: 'feed/liked/<%= maxId ? ("?max_id=" + maxId) : "" %>',
+    storyViewers: 'media/<%= mediaId %>/list_reel_media_viewer/<%= maxId ? ("?max_id=" + maxId) : "" %>',
     locationFeed: 'feed/location/<%= id %>/?<%= maxId ? ("max_id=" + maxId + "&") : "" %>rank_token=<%= rankToken %>',
     followingFeed: 'friendships/<%= id %>/following/?<%= maxId ? ("max_id=" + maxId + "&") : "" %>rank_token=<%= rankToken %>',
     followersFeed: 'friendships/<%= id %>/followers/<%= maxId ? ("?max_id=" + maxId) : "" %>',
@@ -52,12 +55,18 @@ const ROUTES = {
     mediaConfigureSidecar: 'media/configure_sidecar/',
     mediaInfo: 'media/<%= mediaId %>/info/',
     mediaLikes: 'media/<%= mediaId %>/likers/',
-    mediaComments: 'media/<%= mediaId %>/comments/<%= maxId ? ("?max_id=" + maxId) : "" %>',
+    mediaComments: 'media/<%= mediaId %>/comments/?can_support_threading=true<%= minId ? ("&min_id=" + minId) : "" %><%= maxId ? ("&max_id=" + maxId) : "" %>',
     mediaDeletePhoto: 'media/<%= mediaId %>/delete/?media_type=PHOTO',
     mediaEdit: 'media/<%= mediaId %>/edit_media/',
     qeSync: 'qe/sync/',
+    launcherSync: 'launcher/sync/',
+    readMsisdnHeader: 'accounts/read_msisdn_header/',
+    logAttribution: 'attribution/log_attribution/',
+    zeroRatingToken: 'zr/token/result/?custom_device_id=<%=cd_id%>&device_id=<%=d_id%>&fetch_reason=<%=fr%>&token_hash=<%=th%>',
+    contactPointPrefill: 'accounts/contact_point_prefill/',
     discoverAyml: 'discover/ayml/',
-    inbox: 'direct_v2/inbox/<%= cursor ? ("?cursor=" + cursor) : "" %>',
+    exploreFeed: 'discover/explore/?is_prefetch=<%=is_prefetch%>&is_from_promote=false&timezone_offset=2&session_id=<%=session_id%>&supported_capabilities_new=<%=supported_capabilities_new%>&max_id=0&module=explore_popular',
+    inbox: 'direct_v2/inbox/?persistentBadging=true&use_unified_inbox=true<%= cursor ? ("&cursor=" + cursor) : "" %>',
     inboxPending: 'direct_v2/pending_inbox/<%= maxId ? ("?max_id=" + maxId) : "" %>',
     threads: 'direct_v2/threads/?user_ids=<% JSON.stringify(threads) %>',
     threadsShow: 'direct_v2/threads/<%= threadId %>/<%= cursor ? ("?cursor=" + cursor) : "" %>',
@@ -72,7 +81,12 @@ const ROUTES = {
     threadsBrodcastPhoto: 'direct_v2/threads/broadcast/configure_photo/',
     threadsApproveAll: 'direct_v2/threads/approve_all/',
     threadsRecentRecipients: 'direct_share/recent_recipients/',
+    getRankedRecipients: 'direct_v2/ranked_recipients/?mode=<%=mode%>&show_threads=<%=show_threads%>&use_unified_inbox=<%=use_unified_inbox%>',
     autocompleteUserList: 'friendships/autocomplete_user_list/?version=2&followinfo=True',
+    getBootstrapUsers: 'scores/bootstrap/users/?surfaces=<%=surfaces%>',
+    getPresences: 'direct_v2/get_presence/',
+    getRecentActivityInbox: 'news/inbox/',
+    getProfileNotice: 'users/profile_notice/',
     megaphoneLog: 'megaphone/log/',
     block: 'friendships/block/<%= id %>/',
     unblock: 'friendships/unblock/<%= id %>/',
@@ -89,28 +103,28 @@ const WEB_ROUTES = {
     userInfo: '<%= id %>/'
 }
 
-// const PRIVATE_KEY = {
-//     SIG_KEY: '0443b39a54b05f064a4917a3d1da4d6524a3fb0878eacabf1424515051674daa',
-//     SIG_VERSION: '4',
-//     APP_VERSION: '10.33.0'
-// }
-
 const PRIVATE_KEY = {
-    SIG_KEY: '673581b0ddb792bf47da5f9ca816b613d7996f342723aa06993a3f0552311c7d',
+    SIG_KEY: 'ac5f26ee05af3e40a81b94b78d762dc8287bcdd8254fe86d0971b2aded8884a4',
     SIG_VERSION: '4',
-    APP_VERSION: '42.0.0.19.95'
+    APP_VERSION: '64.0.0.14.96'
 }
 
+
 const TLD = 'instagram.com';
-// const HOSTNAME = 'i.instagram.com';
 const HOSTNAME = 'i.instagram.com';
 const WEB_HOSTNAME = 'www.instagram.com';
 const HOST = 'https://' + HOSTNAME + '/';
 const WEBHOST = 'https://' + WEB_HOSTNAME + '/';
 
+const HEADERS = {
+    FB_ANALYTICS_APPLICATION_ID: '567067343352427',
+    X_IG_Connection_Type: 'WIFI',
+    X_IG_Capabilities: '3brTPw=='
+}
 
 module.exports = {
     ROUTES: ROUTES,
+    HEADERS: HEADERS,
     WEB_ROUTES: WEB_ROUTES,
     HOSTNAME: HOSTNAME,
     WEB_HOSTNAME: WEB_HOSTNAME,
@@ -120,5 +134,6 @@ module.exports = {
     API_ENDPOINT: HOST + 'api/v1/',
     EXPERIMENTS: EXPERIMENTS,
     PRIVATE_KEY: PRIVATE_KEY,
-    instagramAgentTemplate: _.template('Instagram <%= version %> Android (<%= agent %>)')
+    instagramAgentTemplate: _.template('Instagram <%= version %> Android (<%= agent %>)'),
+    instagramAgentWebTemplate: _.template('Mozilla/5.0 (Linux; Android <%= release %>; <%= model %> Build/<%= build %>; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.110 Mobile Safari/537.36 <%= instagramAgent %>')
 }
